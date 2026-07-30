afb27eec8cc2923cd5a22f9d11161358f8a776b9
mickeu <mickeu@users.noreply.github.com>
2026-07-30 08:28:19 +0800

    默认策略改为 DIRECT

--- Scripts/PingMe/PingMeSignin.js
+++ Scripts/PingMe/PingMeSignin.js
@@ -13,8 +13,8 @@ const VIDEO_DELAY = 10000;
 
 const NOTIFY_ICON = 'https://raw.githubusercontent.com/axhani/icon/refs/heads/main/pingme&wetalk.png';
 
-// 从模块参数读取策略设置（默认 Proxy）
-const policy = $argument || 'Proxy';
+// 从模块参数读取策略设置（默认 DIRECT）
+const policy = $argument || 'DIRECT';
 
 // 每运行一次重新生成一个固定的伪造设备ID，整次运行所有视频都用同一个设备ID
 const fakeDeviceId = genFakeDeviceId();
