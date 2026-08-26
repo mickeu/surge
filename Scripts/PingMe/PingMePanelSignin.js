9d666517c171a374cb531af69b4e04e677150dc6
Minis <minis@local>
2026-08-26 09:44:58 +0800

    test: PingMe 图标试 arrow.down.message

--- Scripts/PingMe/PingMePanelSignin.js
+++ Scripts/PingMe/PingMePanelSignin.js
@@ -46,7 +46,7 @@ startTasks().then(r => {
         $.done({
             title: "🔄 PingMe 签到",
             content: "点击签到",
-            icon: "pin.point.of.interest.fill",
+            icon: "arrow.down.message",
             "icon-color": "#007AFF"
         });
     } else {
