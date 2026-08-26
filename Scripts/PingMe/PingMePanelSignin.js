b38b09eaeb5bb72d20bc0d7421ecca84d9d40fca
Minis <minis@local>
2026-08-26 09:11:03 +0800

    feat: PingMe 图标改为 pin.point.of.interest 基础版

--- Scripts/PingMe/PingMePanelSignin.js
+++ Scripts/PingMe/PingMePanelSignin.js
@@ -46,7 +46,7 @@ startTasks().then(r => {
         $.done({
             title: "🔄 PingMe 签到",
             content: "点击签到",
-            icon: "mappin",
+            icon: "pin.point.of.interest",
             "icon-color": "#007AFF"
         });
     } else {
